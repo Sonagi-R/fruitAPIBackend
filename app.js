e36ebc2529f5e358ec7e5512cbc9d8a9cfca64e7
Lewis Takavarasha <lewis.takavarasha@gmail.com>
2023-03-01 16:46:01 +0000

    fixed post request

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.get('/fruits', (req, res) => {
 app.get('/fruits/:fruitName', (req, res) =>{
     let {fruitName} = req.params
     fruitName = fruitName.toLowerCase()
-
-
+    //filter through fruits array and return fruit on match
     const response = fruits.filter((fruit) => {
         if(fruitName.toLowerCase() == fruit.name.toLowerCase()){
             return fruit
@@ -36,59 +35,58 @@ app.get('/fruits/:fruitName', (req, res) =>{
 app.post('/fruits', (req, res) => {
     const newFruit = req.body
     let fruitDoesntExist = true
-    console.log(req.body)
-    
+    //looping through fruits array and updating existence variable if the fruit already exists
     for(let i=0;i<fruits.length;i++){
         if (fruits[i].name.toLowerCase() == newFruit.name.toLowerCase()){
             res.send('This fruit already exists')
             return fruitDoesntExist == false
         }
     }
-
-
+    //if the fruit exists, update fruits array and return 201 status with array
     if(fruitDoesntExist){
-        console.log("new entry")
-        fruits.push(newFruit)
+        console.log(`${newFruit.name} was added to the fruits array`)
+        fruits = fruits.push(newFruit)
         res.status(201).send(fruits)
+        return fruits
     }
-    // fruits.filter((fruit) => {
-    //     if(newFruit.name.toLowerCase() == fruit.name.toLowerCase()){
-    //         res.send("This fruit already exists")
-    //     }
-
-    //     if(newFruit.name.toLowerCase() !== fruit.name.toLowerCase()){
-            
-    //     }
-        
-    //  })
+    return fruits
 })
 
 app.delete('/fruits', (req, res) => {
     const deleteFruit = req.body
-
+    //filter fruits and return all fruits except for match, deleting match as a result
      newArray = fruits.filter((fruit) => {
         if(deleteFruit.name.toLowerCase() !== fruit.name.toLowerCase()){
             return fruit
         }
      })
-
+    //if newArray is empty or the same length as fruit array then no change was made, fruit doesnt exist
      if(newArray.length == 0|| newArray.length == fruits.length){
         res.send("The fruit does not exist")
-     } else {res.send(newArray) }    
+     } else {res.send(newArray)}    
 
 
      fruits = newArray
 })
 
-app.patch('/fruits', (req, res) => {
-    const patchFruit = req.body
-   for(let i=0; i<fruits.length; i++){
-        if(patchFruit.name == fruits.name){
-            return i
-        }
-    }
+//patch request to be built
+// app.patch('/fruits', (req, res) => {
+//     const patchFruit = req.body
+//    for(let i=0; i<fruits.length; i++){
+//         if(patchFruit.name == fruits.name){
+//             return i
+//         }
+//         return i
+//     }
+//     let objectArray = Object.entries(fruit[i])
 
-})
+//     for(let j=0;j<objectArray.length;j++){
+//         if(objectArray[i][0] == patchFruit[1][0]){
+//             objectArray[i] = 
+//         }
+//     }
+
+// })
 
 module.exports = app
 
